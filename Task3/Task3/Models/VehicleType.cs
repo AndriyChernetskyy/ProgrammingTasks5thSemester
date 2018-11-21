835a606b4191b4235ad9b57494ea15a3466bff80
Rostyk Supyk <supykrostyslav@gmail.com>
2018-11-21 21:59:24 +0200

    Task 3

---
+++ Task3/Task3/Models/VehicleType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task3.Models
+{
+    /// <summary>
+    /// Types of vehicle
+    /// </summary>
+    public enum VehicleType
+    {
+        Curtainsider,
+        Box,
+        Minibus,
+        Refridgerator,
+        Dropside,
+        Platform,
+        Tank,
+        Special
+    }
+}
